bfd106a04f7462cb3c2ba324940e7529dca644e6
Frederik Wille <frederik@wille.hamburg>
2019-08-21 21:06:07 +0200

    add GET to Polls

--- Makefile
+++ Makefile
@@ -9,6 +9,14 @@ install:
 run:
 	poetry run python main.py
 
+.PHONY: run_api
+run_api:
+	poetry run python api.py
+
 .PHONY: test
 test:
 	poetry run pytest
+
+.PHONY:	initdb
+initdb:
+	poetry run python initdb.py
--- poll_api/resources/poll.py
+++ poll_api/resources/poll.py
@@ -1,8 +1,29 @@
-from flask_restful import Resource
+from flask_restful import Resource, fields, marshal_with
 from pollbot.models import Poll
 from pollbot.db import get_session
 
+poll_resource_fields = {
+    'uuid': fields.String,
+    # 'uri': fields.Url('todo_ep')
+    'name': fields.String,
+    'description': fields.String,
+    'locale': fields.String,
+    'poll_type': fields.String,
+    'number_of_votes': fields.Integer,
+    'anonymous': fields.Boolean,
+    'results_visible': fields.Boolean,
+    'due_date': fields.DateTime(dt_format='iso8601'),
+    'allow_new_options': fields.Boolean,
+    'european_date_format': fields.Boolean,
+    'option_sorting': fields.String,
+    'user_sorting': fields.String,
+    'created': fields.Boolean,
+    'closed': fields.Boolean
+    # TODO: Votes, Options, optional: User
+}
+
 class PollResource(Resource):
+    @marshal_with(poll_resource_fields)
     def get(self):
         # print("debug")
         # return {'asd':'asd'}
@@ -12,5 +33,5 @@ class PollResource(Resource):
             .filter(Poll.closed.is_(False)) \
             .all()
             # .filter(Poll.user == user) TODO
-        print(polls[0])
-        return polls[0]
+        print(polls)
+        return polls
